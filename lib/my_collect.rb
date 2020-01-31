58b8b1056fdba2ff6918811bfb0e89944ca7e4b7
richard dinh <richardndinh@gmail.com>
2020-01-31 03:42:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,5 +2,11 @@ arrays = ["Ruby", "JavaScript", "Python", "Objective-C"]
 
 
 def my_collect(empty_array)
-   
+   i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
